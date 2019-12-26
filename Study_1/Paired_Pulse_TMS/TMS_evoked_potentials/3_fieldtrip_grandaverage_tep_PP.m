3d68d0907bc597337eec520de253be417e823828
tonoplast <tonoplast.chloroplast@gmail.com>
2019-12-27 00:04:09 +1100

    Create 3_fieldtrip_grandaverage_tep_PP.m

---
+++ Study_1/Paired_Pulse_TMS/TMS_evoked_potentials/3_fieldtrip_grandaverage_tep_PP.m
@@ -0,0 +1,81 @@
+clear;
+
+% ##### GRAND AVERAGE TEPS #####
+
+% This script converts single fieldtrip files in to a grand average which
+% is required for input in to the cluster-based permutation statistics
+% script. This is for Paired-Pulse (PP) data.
+%
+% IMPORTANT!!!
+% Before running the script, make sure that the number of id{x,1} is equal 
+% the to number of participants 
+
+% ##### SETTINGS #####
+
+ID = {'H001'; 'H002'; 'H003'; 'H004';'H005';'H006'; 'H007';'H008';'H009';'H010'};
+
+Sesh= {'cTBS'; 'iTBS'; 'SH'};
+tp = {'T0' ; 'T1'};
+
+inPath = 'F:\Data\EEG\0_TBS\TMS_analysis\ROI\ft\';
+PPinPath = 'F:\Data\EEG\0_TBS\TMS_analysis\';
+
+% ##### SCRIPT #####
+
+ft_defaults;
+
+ for z = 1:size(tp,1)
+     
+    for y = 1:size(Sesh,1)
+        
+               for x = 1:size(ID,1)
+     
+
+    filename = [ID{x,1} '_PPTMS_' Sesh{y,1} '_final_' tp{z,1} '_ft'];
+    
+    load([PPinPath filesep ID{x,1} filesep Sesh{y,1} filesep [ID{x,1} '_' Sesh{y,1} '_'  tp{z,1} '_PP_final_corrected']], 'avgPC');
+
+    %load file
+    load([inPath,filename]);
+    %create structure
+    ftData.avg = avgPC;
+    ALL.(ID{x,1}) = ftData;
+
+
+               end
+%%
+
+%Perform grand average
+cfg=[];
+cfg.keepindividual = 'yes';
+
+%IMPOTANT! Check that number of id{x,1} is equal to number of participants
+grandAverage = ft_timelockgrandaverage(cfg,...
+        ALL.(ID{1,1}),...
+        ALL.(ID{2,1}),...
+        ALL.(ID{3,1}),...
+        ALL.(ID{4,1}),...
+        ALL.(ID{5,1}),...
+        ALL.(ID{6,1}),...
+        ALL.(ID{7,1}),...
+        ALL.(ID{8,1}),...
+        ALL.(ID{9,1}),...
+        ALL.(ID{10,1}));
+        
+%Checks that the number of participants is correct
+if size(ID,1) ~= size(grandAverage.individual,1)
+    error('Number of participants in grandAverage does not match number of participants in ID. Data not saved');
+end
+
+%set filename
+filename = ['PPTMS_' Sesh{y,1} '_final_' tp{z,1} '_ft_ga'];
+
+%Save data
+save([inPath,filename],'grandAverage');
+    
+    end
+        
+ 
+ end
+ 
+
